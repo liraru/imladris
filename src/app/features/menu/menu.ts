b163f16cbb16a561c7e6c94a96a695bdd739b033
Lorena Sineiro <losingar@gmail.com>
2026-08-27 17:08:26 +0200

    menu configured, pending solve console errors

--- src/app/features/menu/menu.ts
+++ src/app/features/menu/menu.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { APP_ROUTES } from '../../shared/constants/routes.constant';
-import { NgForOf } from '../../../../node_modules/@angular/common/types/_common_module-chunk';
 
 @Component({
-  imports: [RouterLink, RouterLinkActive, RouterOutlet, MatToolbarModule, MatTabsModule],
+  imports: [RouterLink, RouterLinkActive, MatToolbarModule, MatTabsModule],
   selector: 'app-menu',
   styleUrl: './menu.css',
   templateUrl: './menu.html',
--- src/app/shared/constants/routes.constant.ts
+++ src/app/shared/constants/routes.constant.ts
@@ -7,7 +7,7 @@ enum ROUTES {
   YEARLY_READINGS = 'yearly-readings',
 }
 
-export const APP_ROUTES: { [key in ROUTES]: MenuRoute } = {
+export const APP_ROUTES: { [key: string]: MenuRoute } = {
   HOME: { title: 'Inicio', path: ROUTES.HOME },
   CALCULATOR: { title: 'Calculadora', path: ROUTES.CALCULATOR },
   LIBRARY: { title: 'Biblioteca', path: ROUTES.LIBRARY },
